148ffd91bf2c666f1bb8f48979efc2c83027a6cd
Mark Legkovskis <Marks.Legkovskis@warwick.ac.uk>
2025-01-06 21:20:40 +0000

    DevSecOps

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,13 +13,19 @@ provider "google" {
   region  = "europe-west2"
 }
 
+# New variable to accept environment suffix
+variable "env_suffix" {
+  type    = string
+  default = "dev"
+}
+
 resource "google_service_account" "service" {
-  account_id   = "cloud-run-exec"
+  account_id   = "cloud-run-exec-${var.env_suffix}"
   display_name = "Cloud Run Execution SA"
 }
 
 resource "google_cloud_run_service" "tiny_llm_service" {
-  name     = "tiny-llm-service"
+  name     = "tiny-llm-service-${var.env_suffix}"
   location = "europe-west2"
 
   template {
@@ -37,15 +43,15 @@ resource "google_cloud_run_service" "tiny_llm_service" {
   autogenerate_revision_name = true
 }
 
-# Allow unauthenticated
 resource "google_cloud_run_service_iam_member" "noauth" {
-  location = "europe-west2"
-  project  = "aiops-bone-zone"
+  location = google_cloud_run_service.tiny_llm_service.location
+  project  = google_cloud_run_service.tiny_llm_service.project
   service  = google_cloud_run_service.tiny_llm_service.name
   role     = "roles/run.invoker"
   member   = "allUsers"
 }
 
 output "cloud_run_url" {
-  value = google_cloud_run_service.tiny_llm_service.status[0].url
+  description = "URL of ephemeral Cloud Run service"
+  value       = google_cloud_run_service.tiny_llm_service.status[0].url
 }
